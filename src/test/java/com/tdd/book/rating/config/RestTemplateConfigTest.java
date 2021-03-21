dcda6198505c1a61ceca6d713d2e074c735ef030
Wayne Chan <wchan.it@outlook.com>
2021-03-21 01:35:31 -0500

    Add RestTemplateConifg and its unit test

---
+++ src/test/java/com/tdd/book/rating/config/RestTemplateConfigTest.java
@@ -0,0 +1,29 @@
+package com.tdd.book.rating.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import javax.swing.*;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RestTemplateConfigTest {
+
+    @Autowired
+    private RestTemplateConfig restTemplateConfig;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Test
+    public void shouldHaveValidBeanCreatedForRestTemplate() {
+        assertNotNull("RestTemplate bean is null", restTemplateConfig.restTemplate());
+    }
+
+}
