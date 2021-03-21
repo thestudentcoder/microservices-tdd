73abd673ff621869e524dd3f5eb984fc41e5a0d5
Wayne Chan <wchan.it@outlook.com>
2021-03-21 02:06:26 -0500

    Implemented RatingControlServiceImpl method

--- src/main/java/com/tdd/book/rating/service/RatingControlServiceImpl.java
+++ src/main/java/com/tdd/book/rating/service/RatingControlServiceImpl.java
@@ -1,7 +1,13 @@
 package com.tdd.book.rating.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -16,6 +22,27 @@ public class RatingControlServiceImpl implements RatingControlService{
 
     @Override
     public boolean canReadBook(String customerRatingControlLevel, String bookId) {
+        HttpEntity<?> requestEntity = new HttpEntity<>(generateHeader());
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                "https://my-third-party.service.com/fetch/book/rating" + bookId,
+                HttpMethod.GET,
+                requestEntity,
+                String.class);
+
+        if (HttpStatus.OK == responseEntity.getStatusCode()) {
+            String ratingControlLevel = responseEntity.getBody();
+            if (ratingControlLevel != null) {
+                return Integer.parseInt(ratingControlLevel) <= Integer.parseInt(customerRatingControlLevel);
+            }
+        }
+
         return false;
+    }
+
+    private MultiValueMap<String, String> generateHeader() {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("Accept", "application/json");
+        headers.add("content-type", "application/json");
+        return headers;
     }
 }
